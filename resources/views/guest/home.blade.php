2dccddca86ae7d524a84b0b347f8fd23e18c281e
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-05-21 14:57:22 +0200

    [add] admin

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.guest')
+
+
+@section('content')
+
+    <h1>Home sito Pubblico</h1>
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+
+// rotte Pubbliche (Guest)
+Route::get('/', [PageController::class, 'index'])->name('home');
+
+// rotte Private (Admin)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+// rotte Autenticazione
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
