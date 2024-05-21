778dc7e1fe986957448959204b6f33179f0957e2
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-05-21 16:41:37 +0200

    migrate project

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -12,10 +12,10 @@
             <a class="nav-link active" aria-current="page" href="{{route('admin.home')}}">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">-----</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="#">-----</a>
           </li>
         </ul>
         <span class="navbar-text">
